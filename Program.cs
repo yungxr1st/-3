fbc937b6b2e32714fb909fb87c22217db6dc49b7
Александр Данилов <23san00@gmail.com>
2024-10-07 20:01:04 +0700

    коммит4

--- Program.cs
+++ Program.cs
@@ -8,7 +8,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("исрпо самый лучший предмет");
+            Console.WriteLine("исрпо самый лучший предмет!");
+            Console.Write("Введите размерность матрицы n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] mas = new int[n, n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mas[i, j] = random.Next(0, 10);
+                    Console.Write($"{mas[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x += mas[i, i];
+                y += mas[i, n - i - 1];
+            }
+            Console.WriteLine($"Сумма элементов главной диагонали: {x}");
+            Console.WriteLine($"Сумма элементов побочной диагонали: {y}");
+            if (x > y)
+            {
+                Console.WriteLine("Сумма главной диагонали больше суммы побочной диагонали.");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Сумма побочной диагонали больше суммы главной диагонали.");
+            }
+            else
+            {
+                Console.WriteLine("Суммы главной и побочной диагоналей равны.");
+            }
         }
     }
 }
