ad2a3ee5519ebcb058a2fe17653a71a530244a73
Александр Данилов <23san00@gmail.com>
2024-10-07 19:18:23 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("исрпо самый лучший предмет");
+        }
+    }
+}
