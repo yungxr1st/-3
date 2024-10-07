4acdb9e96d0c2d43e62aaca3c9410c71ac134627
Александр Данилов <23san00@gmail.com>
2024-10-07 19:53:29 +0700

    коммит

--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("исрпо самый лучший предмет");
+            for (int i = 0; i < n; i++)
+            {
+                x += mas[i, i];
+                y += mas[i, n - i - 1];
+            }
+            Console.WriteLine($"Сумма элементов главной диагонали: {x}");
+            Console.WriteLine($"Сумма элементов побочной диагонали: {y}");
+            if (x > y)
+            {
+                Console.WriteLine("Сумма главной диагонали больше суммы побочной диагонали.");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Сумма побочной диагонали больше суммы главной диагонали.");
+            }
+            else
+            {
+                Console.WriteLine("Суммы главной и побочной диагоналей равны.");
+            }
         }
     }
 }
